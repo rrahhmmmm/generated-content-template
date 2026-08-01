001217bd117deb8f1bd0c1c4566d65a308f3221a
rrahhmmmm <muhammadbaihaqi0990@gmail.com>
2026-08-01 16:23:58 +0700

    fix(build): hapus img.crossOrigin, preview template bug

--- src/components/template-editor/preview-canvas.tsx
+++ src/components/template-editor/preview-canvas.tsx
@@ -34,7 +34,6 @@ function drawVideoPlaceholder(ctx: CanvasRenderingContext2D, w: number, h: numbe
 async function loadImage(url: string): Promise<HTMLImageElement> {
   return new Promise((resolve, reject) => {
     const img = new Image();
-    img.crossOrigin = "anonymous";
     img.onload = () => resolve(img);
     img.onerror = () => reject(new Error(`Gagal load: ${url}`));
     img.src = url;
